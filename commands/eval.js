61cf633f82e6b8bbf95934b907b7a9d49c64b337
UnitedStatesBot <49205230+UnitedStatesBot@users.noreply.github.com>
2019-04-02 18:05:06 +0100

    Create eval.js

---
+++ commands/eval.js
@@ -0,0 +1,29 @@
+const bconfig = require(`../config.json`);
+
+exports.run = async (client, message, args) => {
+  function clean(text) {
+    if (typeof (text) === "string") {
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    } else {
+      return text;
+    }
+  }
+  if (message.author.id !== bconfig.ownerID) return;
+
+  try {
+    const code = args.join(" ");
+    let evaled = eval(code);
+
+    if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+
+    message.channel.send(clean(evaled), {
+      code: "js"
+    });
+  } catch (err) {
+    message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+  };
+};
+
+exports.help = {
+  name: "eval"
+};
